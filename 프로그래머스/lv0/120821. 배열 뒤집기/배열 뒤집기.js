0ccf9066fe5e9e68271c0659e9e7a57c6819c86e
LastCarol <104302654+LastCarol@users.noreply.github.com>
2023-03-14 17:56:53 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,8 @@
+function solution(num_list) {
+    let result = [];
+    let count = 0
+    for (let i = (num_list.length-1); i >= 0; i--){
+        result.push ((num_list[i]))
+    }
+    return result
+}
