843e330755e8122d5d3bc2296b1def3e63355a3f
LastCarol <104302654+LastCarol@users.noreply.github.com>
2023-04-27 20:42:02 +0900

    [level 2] Title: 숫자의 표현, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  let answer = 0;
+  
+  for(let i = 0; i <= n; i++) {
+  	if(n%i === 0 && i%2 === 1) answer++;
+  }
+  
+  return answer;
+}
+
+// test
+console.log(solution(15)); // 4
