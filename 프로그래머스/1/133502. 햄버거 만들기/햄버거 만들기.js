3dc5bb20bb328036081a4c3730db6e63e23fc9e6
LastCarol <104302654+LastCarol@users.noreply.github.com>
2025-01-24 20:41:33 +0900

    [level 1] Title: 햄버거 만들기, Time: 238.68 ms, Memory: 97.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,19 @@
+function solution(ingredient) {
+    let answer = 0;
+    const stack = [];
+    
+    for (let i = 0; i < ingredient.length; i++) {
+        stack.push(ingredient[i]);
+        
+        if (stack.length >= 4) {
+            const burger_ingredient = stack.slice(-4).join("");
+            
+            if (burger_ingredient === '1231') {
+                stack.splice(-4);
+                answer += 1;
+            }
+        }
+    }
+    
+    return answer;
+}
